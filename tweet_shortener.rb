5f733f45ceb8a6c4cb148c1e95d954de25737c74
Leah Boyd <leahboyd08@gmail.com>
2019-03-22 15:35:36 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def dictionary
+  substituted_words = {
+    "hello" => "hi",
+    "to"=> "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet_string)
+  tweet_array = []
+  tweet_array << tweet_string.split(", ")
+  tweet_array.each_with_index do |tweet_string, index|
+    substituted_words.each_key do |word|
+      if tweet_string.inlcude?(word)
+        tweet_array[index].gsub!(word, substituted_words[word])
+        tweet_array.join(" ")
+    end
+  end
+end
